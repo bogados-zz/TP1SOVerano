8c9c6472a58c2e5f7a25f202baf661507b065bd9
sbogado <seebogado@hotmail.com>
2016-02-21 12:51:58 -0300

    Se agrega el atributo cantidad que es lo que no se sabía que era, y ademas, el log se agrega al final. No se sobreescribe mas

--- Ejercicio2/Ejercicio2.ps1
+++ Ejercicio2/Ejercicio2.ps1
@@ -5,8 +5,8 @@ El script copia los archivos de texto que contengan una cadena determinada a un 
 .DESCRIPTION
 El script copia los archivos de texto que contengan una cadena determinada a un directorio destino
 Para ello, se necesita tener el directorio origen del archivo txt a copiar, directorio destino y la cadena que se quiera buscar en los archivos
-Al finalizar la copia, se debe crear un archivo de log en donde se indique La fecha y hora de ejecuciÃ³n del script,
-el directorio de origen, el tamaÃ±o y la fecha de modificaciÃ³n de cada uno de los archivos copiados
+Al finalizar la copia, se debe crear un archivo de log en donde se indique La fecha y hora de ejecución del script,
+el directorio de origen, el tamaño y la fecha de modificación de cada uno de los archivos copiados
 y La cantidad de veces que se repite la palabra dentro del archivo.
 
 .PARAMETER cadenaABuscar
@@ -58,7 +58,7 @@ if($path1 -eq $true -and $path2 -eq $true){
     #$directorio = Get-ChildItem -Path $origen -Recurse | Where {$_.psIsContainer -eq $false}<#$true si quiero listar directorio#>
     $directorio = ls -Path $origen -Recurse -Include "*.txt"
     $fechaYHoraScript = Get-Date
-    $fechaYHoraScript >$destino\archivoLog.log
+    $fechaYHoraScript >> $destino\archivoLog.log
 
     foreach($file in $directorio){
         $cant = 0
@@ -68,14 +68,16 @@ if($path1 -eq $true -and $path2 -eq $true){
             if($linea -match $cadena){
                 $cant++
                 $copio = $true
-            }
-            else{}        
+            }    
         }
         if($copio -eq $true){
             cp $file -Destination $destino
             $new = dir -Path $file| Format-List -Property Directory, Length, LastWriteTime
-            $new,$cant >> $destino\archivoLog.log #$new, $cant concateno al escribir
-        
+            $customObject= New-Object psobject
+            Add-Member -InputObject $customObject NoteProperty -Name "cantidad" -Value $cant.ToString()
+
+            $cantidad= $customObject|Format-List -Property cantidad
+            $new,$cantidad >> $destino\archivoLog.log #$new, $cant concateno al escribir       
             }
     }
 }
